2426163a00c397bdc77d9fdd7a8fde14f4d6372b
Alcides Rodrigues <90933223+AlcidesFh@users.noreply.github.com>
2025-07-21 17:07:27 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -33,7 +33,7 @@ document.getElementById("formConfirmacao").addEventListener("submit", function(e
   }
 
   const msg = `Olá! Eu confirmo minha presença no casamento de Alcides e Andreza. Meu nome é ${nome}.`;
-  const numeroDestino = "5511988339183"; // ex: 5511999999999
+  const numeroDestino = "5511981284618"; // ex: 5511999999999
   const url = `https://wa.me/${numeroDestino}?text=${encodeURIComponent(msg)}`;
 
   localStorage.setItem("confirmado_" + telefone, true);
